4270c5da6593a9479a513fda819d98a5bb2ad47d
Franco Nieva <franconieva86@gmail.com>
2023-07-17 18:09:11 +0200

    episode slice added

---
+++ src/app/services/episodes.ts
@@ -0,0 +1,57 @@
+import { EntityState, createEntityAdapter } from '@reduxjs/toolkit';
+import { api } from './api';
+
+export interface EpisodeProps {
+	trackId?: number;
+	trackName?: string;
+	trackTimeMillis?: number;
+	trackViewUrl?: string;
+	id?: number | string;
+	description?: string;
+	episodeUrl?: string;
+	resultsCount?: number;
+}
+
+type GetEpisodesArgs = { id?: string };
+
+type EpisodesResponse = {
+	resultCount: number;
+	results: EpisodeProps[];
+};
+
+const episodesAdapter = createEntityAdapter<EpisodeProps>({});
+
+const initialState = episodesAdapter.getInitialState();
+
+export const episodesApi = api.injectEndpoints({
+	endpoints: (build) => ({
+		getEpisodes: build.query<EntityState<EpisodeProps>, GetEpisodesArgs>({
+			query: ({ id }) => ({
+				url: `/lookup?id=${id}&media=podcast&entity=podcastEpisode`,
+			}),
+			keepUnusedDataFor: 24 * 60 * 60,
+			transformResponse: (responseData: EpisodesResponse) => {
+				let loadedEpisodes = responseData.results.map((episode: EpisodeProps) => {
+					episode.id = episode.trackId;
+					return episode;
+				});
+
+				loadedEpisodes = [
+					...loadedEpisodes,
+					{ id: 'resultsCount', resultsCount: responseData.resultCount },
+				];
+				return episodesAdapter.setAll(initialState, loadedEpisodes);
+			},
+			providesTags: (result) => {
+				if (result?.ids) {
+					return [
+						{ type: 'Episodes', id: 'LIST' },
+						...result.ids.map((id: any) => ({ type: 'Episodes' as const, id })),
+					];
+				} else return [{ type: 'Episodes' as const, id: 'LIST' }];
+			},
+		}),
+	}),
+});
+
+export const { useGetEpisodesQuery } = episodesApi;
